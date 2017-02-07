708b771298c21cba230a5ec50f46f916410246e4
Robert Harries <robertpharries@gmail.com>
2017-02-07 12:05:55 +1100

    Setup the basic project
    Created project, added code to run async on the api call

---
+++ DotaApiManage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotaApiManage
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri()
+        }
+    }
+}
